9dea43b723f7c45d9361683ebf44f45309a5c309
JK Lacrym0sa <artyom.byzov.00@gmail.com>
2024-08-22 22:44:57 +0300

    111

--- App/Program.cs
+++ App/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Handlers;
+using System.ComponentModel.Design;
 
 IBaskovRepository baskovRepository = new BaskovMemoryRepository();
 BaskovHandler baskovHandler = new(baskovRepository);
@@ -8,7 +9,7 @@ while (true)
     var action = Console.ReadLine().ToLower().Trim();
     Console.WriteLine();
 
-    if(action == "get")
+    if (action == "get")
     {
         var baskov = baskovHandler.GetAll();
         if (baskov == null || baskov.Count == 0)
@@ -17,6 +18,25 @@ while (true)
             Console.WriteLine();
             continue;
         }
-
+        foreach (var item in baskov)
+        {
+            Console.WriteLine($"{item.Name} {item.Age} {item.Created}");
+        }
     }
+    else if (action == "add")
+    {
+        Console.WriteLine("Введите имя и возраст");
+        var name = Console.ReadLine();
+        var age = int.Parse(Console.ReadLine());
+
+        var baskovInput = new BaskovInput(name, age);
+
+        baskovHandler.AddBaskov(baskovInput);
+    }
+    else
+    {
+        Console.WriteLine("ебобо?");
+        continue;
+    }
+    Console.WriteLine();
 }
